56f5c5a16ae94b80b9d183fde7dc3f7b9d734d39
michaellgans <michaellgans@hotmail.com>
2023-07-10 07:43:30 -0700

    M - fixed compilation errors, working on betty

--- printf.c
+++ printf.c
@@ -5,34 +5,36 @@
 #include "main.h"
 
 /**
+ * struct structNickname - formats the argument based on the char after %.
+ *
  * _printf - Prints user input.
  * @format: Specifies the format of the input given.
  * Return: Length of string printed.
  */
-
 int _printf(const char *format, ...)
 {
 	/* Define Variables */
 	va_list args;
-	int x, y, length;
+	int x;
+	size_t y;
 	char operationPointer;
-	structNickname structArray;
-
+	struct formatStructure structArray[] = {
+		{"c", formatCharacter},
+		{NULL, NULL}
+	};
 	/* Initializing Varaibles */
 	x = 0;
-	length = strlen(format);
 	va_start(args, format);
-
+	/* Search for % */
 	while (format[x] != '\0')
 	{
 		if (format[x] == '%')
 		{
 			/* Looking at character after % */
 			operationPointer = (format[x + 1]);
-
-			for (y = 0; y < sizeof(structArray) / sizeof(structArray[0]); y++)
+			for (y = 0; structArray[y].operationPointer != NULL; y++)
 			{
-				if (structArray[y].operationPointer == operationPointer)
+				if (strcmp(structArray[y].operationPointer, &operationPointer) == 0)
 				{
 					structArray[y].functionPointer(args);
 					break;
@@ -42,14 +44,11 @@ int _printf(const char *format, ...)
 			x++;
 		}
 		else
-		{
 			putchar(format[x]);
-		}
 		x++;
 	}
 	/* Finish looking for next variable */
 	va_end(args);
-
 	/* Return length of string printed */
-	return (length);
+	return (x);
 }
