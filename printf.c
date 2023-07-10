88ac6ed64a085de8323a6c1e27d451c92cd31cc5
michaellgans <michaellgans@hotmail.com>
2023-07-10 13:51:50 -0700

    M - added comments

--- printf.c
+++ printf.c
@@ -8,6 +8,7 @@
 
 int _printf(const char *format, ...)
 {
+	/* Array of Structs */
 	Nickname a[] = {
 		{"%s", formatString},
 		{"%c", formatCharacter},
@@ -15,25 +16,30 @@ int _printf(const char *format, ...)
 		{"%d", formatNumber},
 		{"%%", print_37}
 	};
-
+	/* Gives input to the macro */
 	va_list args;
 	int x = 0, y, length = 0;
-
+	/* Starts the macro at the last known VAR */
 	va_start(args, format);
-
+	/* If NULL or single %, return -1, indicating Error */
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 Here:
+	/* Look for end of string */
 	while (format[x] != '\0')
 	{
 		y = 4;
 		while (y >= 0)
 		{
-			if (a[y].convPtr[0] == format[x] && a[y].convPtr[1] == format[x + 1])
+			/* Looks at 2 characters at a time. */
+			if (a[y].convPtr[0] == format[x]
+				&& a[y].convPtr[1] == format[x + 1])
 			{
+				/* Perform function on VAR */
 				length += a[y].funcPtr(args);
 				x = x + 2;
+				/* Start OUTER loop again without finishing inner */
 				goto Here;
 			}
 			y--;
@@ -42,6 +48,8 @@ Here:
 		length++;
 		x++;
 	}
+	/* Stops variadic process */
 	va_end(args);
+	/* Returns length of printed characters */
 	return (length);
 }
